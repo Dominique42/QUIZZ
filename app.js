286826d498b1d44330aff16e85fbacd37c589b4b
Dominique42 <dominique.matricon@free.fr>
2025-05-14 12:56:42 +0200

    14/05

---
+++ app.js
@@ -0,0 +1,62 @@
+//j'affiche une question
+//les 4 reponses possibles
+//l'utilisateur choisit une réponse
+//Comparer la réponse de l'utilisateur avec la réponse que l'on attends
+//si egale "bravo"
+//si différente "perdu"
+//On passe à la question suivante
+
+
+
+
+let quizz = [
+    {
+        question : "qui a gagné le mans moto 2025?",
+
+        reponse: " zarco",
+        reponse2: " marquez",
+        reponse 3 " quartararo",
+        reponse 4 " acosta",
+        bonne reponse: reponse 1
+};
+
+
+
+let quizz =
+    {
+        question : "qui a été ballon d'or?",
+
+        reponse 1 "casoni ",
+        reponse 2 " platini ",
+        reponse 3 " jeremy",
+        reponse 4 " milla",
+
+        bonne reponse: reponse 2
+
+};
+
+
+let quizz =
+    {
+        question : "qui sera certifie à la toile?",
+
+        reponse 1 "jeremy";
+        reponse 2 "jean-louis";
+        reponse 3 "Pierre";
+        reponse 4 "lucie";
+
+        bonne reponse 1
+
+};
+
+]
+
+tableau.quizz.forEach(reponses => {
+    let zarco prompt (true)
+
+});
+
+console.log (quizz)
+
+
+
---
+++ test.js
@@ -0,0 +1,47 @@
+let quizz = [
+    {
+        question: "quelle est la couleur du ciel",
+        a: "rouge",
+        b: "vert",
+        c: "blue",
+        d: "rose",
+        vraiReponse: "c"
+    },
+    {
+        question: "2 quel est la couleur ",
+        a: "rouge",
+        b: "vert",
+        c: "blue",
+        d: "rose",
+        vraiReponse: "c"
+    },
+]
+
+//j'affiche la premiere question
+quizz.forEach.question => {
+    //l'utilisateur choisit une réponse (prompt)
+    let reponse = prompt(`${question.question}`)
+    //Comparer la réponse de l'utilisateur avec la réponse que l'on attends
+    
+
+//si egale "bravo"
+
+
+//si différente "perdu"
+
+
+}
+
+
+
+
+
+
+
+
+
+
+//On passe à la question suivante
+
+//les 4 reponses possibles
+
